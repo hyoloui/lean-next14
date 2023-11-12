a1736d47672187eceff280a40a8b3397893d53e8
hyoloui <tmdgy9272@gmail.com>
2023-11-12 15:40:01 +0900

    feat: apply try catch from server actions
    - actions 파일에 있는 함수를 모두 try catch 적용하여 에러 메세지를 작성합니다
    
    Handling Errors #11

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -38,13 +38,19 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
 
-  await sql`
+  try {
+    await sql`
   INSERT INTO invoices (customer_id, amount, status, date)
   VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
 `;
 
-  revalidatePath("/dashboard/invoices");
-  redirect("/dashboard/invoices");
+    revalidatePath("/dashboard/invoices");
+    redirect("/dashboard/invoices");
+  } catch (error) {
+    return {
+      message: "DB 에러: Invoice를 생성할 수 없습니다.",
+    };
+  }
 }
 
 const UpdateInvoice = InvoiceSchema.omit({ date: true, id: true });
@@ -66,11 +72,17 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await sql`
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+  } catch (error) {
+    return {
+      message: "DB 에러: Invoice를 업데이트할 수 없습니다.",
+    };
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
@@ -82,7 +94,13 @@ export async function updateInvoice(id: string, formData: FormData) {
  * @returns revalidate /dashboard/invoices
  */
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+  } catch (error) {
+    return {
+      message: "DB 에러: Invoice를 삭제할 수 없습니다.",
+    };
+  }
 
   revalidatePath("/dashboard/invoices");
 }
